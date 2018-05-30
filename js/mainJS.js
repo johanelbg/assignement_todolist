d18f0824ff3c3ce1e5e37911685e9cfb3183cc50
johan <johan.elberg38@gmail.com>
2018-05-30 21:42:38 +0300

    added done button

--- js/mainJS.js
+++ js/mainJS.js
@@ -2,24 +2,54 @@ class List extends React.Component {
     constructor(props) {
         super(props);
         this.deleteTask = this.deleteTask.bind(this)
+        this.doneTask = this.doneTask.bind(this)
         this.state = {
+            arrDone: []
         };
     }
 
     deleteTask(event){
         event.target.parentElement.remove()
+        // on laisse tomber de suppr l'element de l'array pour le moment , au final pas vraiment utile
+    }
+
+    doneTask(event){
+        // remove a mettre ici je pense
+        var tempString = event.target.parentElement.textContent
+        var doneText = tempString.slice(0, -3) // ca permet de suppr le'X D' des boutons dans la string
+        this.state.arrDone.push(doneText)
+        console.log(this.state.arrDone)
+        // le but maintenant c'est de choper la nouvelle array dans une autre class faire une map et qu'il soit enfant de APP
+        // jsp si c'est possible !  jvois pas comment faire le lien entre done et list
     }
 
     render() {
         return(
             <ul> 
                 {(this.props.arrTask).map((taskName, i) => <li key={i}>{taskName}
-                    <button onClick={this.deleteTask}>X</button>
+                    <button onClick={this.deleteTask}>X</button> <button onClick={this.doneTask}>D</button>
                 </li>)}
             </ul>
         );
     }
 }
+
+// class Done extends React.Component {
+//     constructor(props) {
+//         super(props);
+//     }
+
+//     render() {
+//         return(
+//             <ul> 
+//                 {(this.props.arrDone).map((test, i) => 
+//                 <li key={i}>
+//                     {test}
+//                 </li>)}
+//             </ul>
+//         );
+//     }
+// }
 
 class App extends React.Component {
       constructor(props) {
@@ -47,7 +77,8 @@ class App extends React.Component {
         return(
             <div>
                 <input onKeyPress={this.enterTask} placeholder='Insert a task' ref={(input) => { this.taskInput = input; }}/>
-                <List arrTask={this.state.arrTask}/>            
+                <List arrTask={this.state.arrTask}/>       
+                {/* Done class ici  */}
             </div>
         );
     }
