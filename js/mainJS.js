ecc2084522ef953c93b0304c39160a9161c1f748
Albert <albertlouzon@gmail.com>
2018-05-31 01:06:19 +0300

    added a first interaction between the ToDo list and the Done list (counter)

--- js/mainJS.js
+++ js/mainJS.js
@@ -3,36 +3,54 @@ class List extends React.Component {
         super(props);
         this.deleteTask = this.deleteTask.bind(this)
         this.doneTask = this.doneTask.bind(this)
-        this.state = {
-            arrDone: []
-        };
+        this.state={
+            arrDone:[],
+            salope:[] 
+        }
     }
 
     deleteTask(event){
-        event.target.parentElement.remove()
+        event.target.parentElement.remove() // Mettre du Fade In
         // on laisse tomber de suppr l'element de l'array pour le moment , au final pas vraiment utile
     }
 
     doneTask(event){
-        // remove a mettre ici je pense
+    
+        event.target.parentElement.remove()  //mettre du Fade Out
         var tempString = event.target.parentElement.textContent
         var doneText = tempString.slice(0, -3) // ca permet de suppr le'X D' des boutons dans la string
-        this.state.arrDone.push(doneText)
-        console.log(this.state.arrDone)
+        this.setState({
+            salope : this.state.arrDone.push(doneText) 
+        })
+        console.log("arrDone :" + this.state.arrDone) 
+
         // le but maintenant c'est de choper la nouvelle array dans une autre class faire une map et qu'il soit enfant de APP
         // jsp si c'est possible !  jvois pas comment faire le lien entre done et list
     }
 
     render() {
         return(
+        <div>
             <ul> 
                 {(this.props.arrTask).map((taskName, i) => <li key={i}>{taskName}
                     <button onClick={this.deleteTask}>X</button> <button onClick={this.doneTask}>D</button>
                 </li>)}
             </ul>
+            
+            <br/>
+            <br/>
+            <br/>
+            {this.state.arrDone} ca il comprend pas
+            <br/>
+            <br/>
+            {this.state.salope} ca il comprend mal
+            <br/>
+       
+        </div>
         );
     }
 }
+
 
 // class Done extends React.Component {
 //     constructor(props) {
@@ -77,7 +95,13 @@ class App extends React.Component {
         return(
             <div>
                 <input onKeyPress={this.enterTask} placeholder='Insert a task' ref={(input) => { this.taskInput = input; }}/>
-                <List arrTask={this.state.arrTask}/>       
+                <List arrTask={this.state.arrTask}> 
+               
+                </List>
+
+
+
+                         
                 {/* Done class ici  */}
             </div>
         );
@@ -86,5 +110,5 @@ class App extends React.Component {
 
 ReactDOM.render(
     <App/>,
-    document.getElementById("root")
+    document.getElementById("root") //TOUT PART DE LA
 );
