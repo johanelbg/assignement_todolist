d06f0a92ee9c6faef5e31c6aa4acbe5b40af9dfc
johan <johan.elberg38@gmail.com>
2018-05-30 16:42:07 +0300

    added class list and app

--- js/mainJS.js
+++ js/mainJS.js
@@ -1,15 +1,43 @@
-class App extends React.Component {
+class List extends React.Component {
     constructor(props) {
         super(props);
-
-        this.state = {};
-
+        this.state = {
+        };
     }
+    render() {
+        return(
+            <ul> 
+                {(this.props.arrTask).map((taskName, i) => <li key={i}>{taskName}</li>)}
+            </ul>
+        );
+    }
+}
+
+class App extends React.Component {
+      constructor(props) {
+        super(props);
+        this.enterTask = this.enterTask.bind(this)
+        this.state = {
+            dynamicTask:"",
+            arrTask:[]
+        };
+    }
+
+    enterTask(event){
+        if(event.key == 'Enter'){
+            this.setState({
+                dynamicTask: event.target.value,
+            })
+            this.state.arrTask.push(event.target.value)
+            console.log(this.state.arrTask)
+        }   
+    } 
 
     render() {
         return(
             <div>
-
+                <input onKeyPress={this.enterTask} placeholder='Insert a task' ref={(input) => { this.taskInput = input; }}/>
+                <List arrTask={this.state.arrTask}/>            
             </div>
         );
     }
